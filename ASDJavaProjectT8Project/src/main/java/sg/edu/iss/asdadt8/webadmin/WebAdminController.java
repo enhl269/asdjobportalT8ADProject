1f6dfcd28b6d5e358e90f92a346e9f1e2a801c10
tansizheng89 <e0696890@u.nus.edu>
2021-08-07 15:37:56 +0800

    change from post to putmapping

--- ASDJavaProjectT8Project/src/main/java/sg/edu/iss/asdadt8/webadmin/WebAdminController.java
+++ ASDJavaProjectT8Project/src/main/java/sg/edu/iss/asdadt8/webadmin/WebAdminController.java
@@ -5,14 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import sg.edu.iss.asdadt8.domain.Applicant;
 
+@CrossOrigin(origins= "http://localhost:3000")
 @RestController
 @RequestMapping("/api/webadmin")
 public class WebAdminController {
@@ -20,7 +23,7 @@ public class WebAdminController {
 	@Autowired
 	UserService uservice;
 	
-	@GetMapping("list")
+	@GetMapping("/list")
 	public List<Applicant> allApplicant(){
 		return uservice.findAllApplicant();
 	}
@@ -35,7 +38,7 @@ public class WebAdminController {
 		return uservice.findAllApplicantByBlockStatus();
 	}
 	
-	@PostMapping("list/applicant/{id}/{status}")
+	@PutMapping("/list/applicant/{id}/{status}")
 	public ResponseEntity<HttpStatus> updateApplicantStatus(@PathVariable("id") Long id, @PathVariable("status") String status){
 		try {
 			uservice.updateApplicantStatus(id,status);
