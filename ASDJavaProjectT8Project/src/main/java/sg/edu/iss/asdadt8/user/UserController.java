3c0e7d616c42be5eeb103c5ca190ffb07613b89c
zhangr4 <ran.zhang863@hotmail.com>
2021-08-08 17:59:14 +0800

    add spring security configuration, currently permit all request

--- ASDJavaProjectT8Project/src/main/java/sg/edu/iss/asdadt8/user/UserController.java
+++ ASDJavaProjectT8Project/src/main/java/sg/edu/iss/asdadt8/user/UserController.java
@@ -1,5 +1,42 @@
 package sg.edu.iss.asdadt8.user;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import sg.edu.iss.asdadt8.domain.User;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/users")
+	public ResponseEntity<List<User>> getUsers(){
+	    return ResponseEntity.ok().body(userService.getUsers());
+	}
+	
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+    
+    @DeleteMapping("/user/delete")
+    public void deleteUser(@RequestBody User user) {
+    	userService.deleteUser(user);
+    }
+    
 }
